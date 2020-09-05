5c85c6fa66d02e656800d7127f50acc05c4a6aa7
silverqx <silver.zachara@gmail.com>
2020-09-05 20:53:50 +0200

    added precompiled headers and exe manifest

---
+++ pch.h
@@ -0,0 +1,65 @@
+/* This file can't be included in the project, it's for a precompiled header. */
+
+/* Add C includes here */
+
+#if defined __cplusplus
+/* Add C++ includes here */
+#include <QFileDialog>
+#include <QPushButton>
+#include <QLabel>
+#include <QCheckBox>
+#include <QComboBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QDrag>
+#include <QMimeData>
+#include <QToolButton>
+#include <QFrame>
+#include <QValidator>
+#include <QAction>
+#include <QButtonGroup>
+#include <QHeaderView>
+#include <QLineEdit>
+#include <QSpacerItem>
+#include <QStackedWidget>
+#include <QRadioButton>
+#include <QTabWidget>
+#include <QToolTip>
+#include <QMouseEvent>
+#include <QStyle>
+#include <QTimer>
+
+#include <QApplication>
+#include <QVariant>
+#include <QMap>
+#include <QVector>
+#include <QStringList>
+#include <QDir>
+#include <QPointer>
+#include <QColor>
+
+#include <string>
+#include <set>
+#include <map>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <array>
+#include <bitset>
+#include <initializer_list>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <type_traits>
+#include <cmath>
+#include <cassert>
+#include <cfloat>
+#include <complex>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+#endif
