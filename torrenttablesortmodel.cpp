8d68e95bdd6004c3b92eadb59053fd497d5b6cee
silverqx <silver.zachara@gmail.com>
2022-02-03 16:10:16 +0100

    revisited torrenttablesortmodel

--- torrenttablesortmodel.cpp
+++ torrenttablesortmodel.cpp
@@ -3,11 +3,6 @@
 #include <QDateTime>
 
 #include "common.h"
-#include "torrentsqltablemodel.h"
-
-TorrentTableSortModel::TorrentTableSortModel(QObject *parent)
-    : QSortFilterProxyModel(parent)
-{}
 
 namespace
 {
@@ -22,75 +17,100 @@ namespace
             TorrentSqlTableModel::TR_SEEDS,
             TorrentSqlTableModel::TR_LEECHERS,
         };
-        // Don't sort by columns in the vector
+
+        // Exclude sorting columns defined in the vector
         return !cached.contains(column);
     };
-}
+} // namespace
 
-bool TorrentTableSortModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
+bool TorrentTableSortModel::lessThan( // NOLINT(misc-no-recursion)
+        const QModelIndex &left, const QModelIndex &right) const
 {
-    const auto invokeLessThanForColumn = [this, &left, &right](const int column) -> bool
-    {
-        return lessThan(left.sibling(left.row(), column), right.sibling(right.row(), column));
-    };
+    const auto sortColumn = left.column();
 
-    const int sortColumn = left.column();
+    // UnderlyingDataRole contains raw value from db
+    const auto leftRawValue = left.data(TorrentSqlTableModel::UnderlyingDataRole);
+    const auto rightRawValue = right.data(TorrentSqlTableModel::UnderlyingDataRole);
 
-    const QVariant leftRawValue = left.data(TorrentSqlTableModel::UnderlyingDataRole);
-    const QVariant rightRawValue = right.data(TorrentSqlTableModel::UnderlyingDataRole);
-    // If not valid or is the same value, than sort by added on column.
-    // It is like second sort, but only if items have the same values.
-    if (!leftRawValue.isValid() || !rightRawValue.isValid()
-        || (sortByAddedOn(sortColumn) && (leftRawValue == rightRawValue)))
-        return invokeLessThanForColumn(TorrentSqlTableModel::TR_ADDED_ON);
+    /* If not valid or is the same value, then sort by added on column.
+       It's something like a second sort, but only if items have the same values. */
+    if (!leftRawValue.isValid() || !rightRawValue.isValid() ||
+        (sortByAddedOn(sortColumn) && leftRawValue == rightRawValue)
+    )
+        return invokeLessThanForColumn(TorrentSqlTableModel::TR_ADDED_ON, left, right);
 
     switch (sortColumn) {
     case TorrentSqlTableModel::TR_ADDED_ON:
         return leftRawValue.toDateTime() < rightRawValue.toDateTime();
+
     case TorrentSqlTableModel::TR_SIZE:
     case TorrentSqlTableModel::TR_AMOUNT_LEFT:
         return leftRawValue.toULongLong() < rightRawValue.toULongLong();
+
     case TorrentSqlTableModel::TR_PROGRESS:
         return leftRawValue.toUInt() < rightRawValue.toUInt();
-    case TorrentSqlTableModel::TR_ETA: {
-        auto leftValue = leftRawValue.toLongLong();
-        auto rightValue = rightRawValue.toLongLong();
-        /* Interpret values larger that max ETA cap as 0, to avoid always being sorted
-           on top / bottom. */
-        if (leftValue >= ::MAX_ETA)
-            leftValue = 0;
-        if (rightValue >= ::MAX_ETA)
-            rightValue = 0;
-        /* Here for performance reason, it could be all above, after QVariant declaration,
-           but I don't want to do this MAX_ETA cap checks for every sort, so this second sorting
-           by TR_ADDED_ON column have to be here again. */
-        if (leftRawValue == rightRawValue)
-            return invokeLessThanForColumn(TorrentSqlTableModel::TR_ADDED_ON);
-        return leftValue < rightValue;
-    }
+
+    case TorrentSqlTableModel::TR_ETA:
+        return sortETA(left, right, leftRawValue, rightRawValue);
+
     case TorrentSqlTableModel::TR_SEEDS:
-    case TorrentSqlTableModel::TR_LEECHERS: {
-        const auto leftValue = leftRawValue.toInt();
-        const auto rightValue = rightRawValue.toInt();
-        // Active seeds/leechers take precedence over total seeds/leechers
-        if (leftValue != rightValue)
-            return (leftValue < rightValue);
-
-        // Second sort by total seeds/leechers
-        // Obtain Total seeds/leechers from sibling column
-        const auto leftValueTotal =
-                left.sibling(left.row(),TorrentSqlTableModel::mapToTotal.value(sortColumn))
-                .data(TorrentSqlTableModel::UnderlyingDataRole).toInt();
-        const auto rightValueTotal =
-                right.sibling(right.row(), TorrentSqlTableModel::mapToTotal.value(sortColumn))
-                .data(TorrentSqlTableModel::UnderlyingDataRole).toInt();
-        if (leftValueTotal != rightValueTotal)
-            return (leftValueTotal < rightValueTotal);
-
-        // Third sort by added on
-        return invokeLessThanForColumn(TorrentSqlTableModel::TR_ADDED_ON);
-    }
+    case TorrentSqlTableModel::TR_LEECHERS:
+        return sortSeedsAndLeechers(left, right, leftRawValue, rightRawValue);
     }
 
     return QSortFilterProxyModel::lessThan(left, right);
+}
+
+bool TorrentTableSortModel::sortETA( // NOLINT(misc-no-recursion)
+        const QModelIndex &left, const QModelIndex &right,
+        const QVariant &leftRawValue, const QVariant &rightRawValue) const
+{
+    auto leftValue = leftRawValue.toLongLong();
+    auto rightValue = rightRawValue.toLongLong();
+
+    /* Interpret values larger than max. ETA cap as 0, to avoid always being sorted
+       on top / bottom. */
+    if (leftValue >= ::MAX_ETA)
+        leftValue = 0;
+    if (rightValue >= ::MAX_ETA)
+        rightValue = 0;
+
+    /* Here for performance reason, it could be all above, after QVariant declaration,
+       but I don't want to do this MAX_ETA cap checks for every sort, so this second
+       sorting by TR_ADDED_ON column have to be here again. */
+    if (leftRawValue == rightRawValue)
+        return invokeLessThanForColumn(TorrentSqlTableModel::TR_ADDED_ON, left, right);
+
+    return leftValue < rightValue;
+}
+
+bool TorrentTableSortModel::sortSeedsAndLeechers( // NOLINT(misc-no-recursion)
+        const QModelIndex &left, const QModelIndex &right,
+        const QVariant &leftRawValue, const QVariant &rightRawValue) const
+{
+    const auto leftValue = leftRawValue.toInt();
+    const auto rightValue = rightRawValue.toInt();
+
+    // Active seeds/leechers take precedence over total seeds/leechers
+    if (leftValue != rightValue)
+        return leftValue < rightValue;
+
+    /* Second sort by total seeds/leechers. Obtain Total seeds/leechers from
+       a sibling column. */
+    const auto sortColumn = left.column();
+
+    const auto leftValueTotal =
+            left.sibling(left.row(),
+                         TorrentSqlTableModel::mapToTotal.value(sortColumn))
+            .data(TorrentSqlTableModel::UnderlyingDataRole).toInt();
+    const auto rightValueTotal =
+            right.sibling(right.row(),
+                          TorrentSqlTableModel::mapToTotal.value(sortColumn))
+            .data(TorrentSqlTableModel::UnderlyingDataRole).toInt();
+
+    if (leftValueTotal != rightValueTotal)
+        return leftValueTotal < rightValueTotal;
+
+    // Third sort by added on
+    return invokeLessThanForColumn(TorrentSqlTableModel::TR_ADDED_ON, left, right);
 }
--- torrenttablesortmodel.h
+++ torrenttablesortmodel.h
@@ -4,16 +4,41 @@
 
 #include <QSortFilterProxyModel>
 
+#include "torrentsqltablemodel.h"
+
 class TorrentTableSortModel final : public QSortFilterProxyModel
 {
     Q_OBJECT
     Q_DISABLE_COPY(TorrentTableSortModel)
 
 public:
-    explicit TorrentTableSortModel(QObject *parent = nullptr);
+    /*! Inherit constructors. */
+    using QSortFilterProxyModel::QSortFilterProxyModel;
 
 private:
-    bool lessThan(const QModelIndex &left, const QModelIndex &right) const override;
+    /*! Main comparison method. */
+    bool lessThan(const QModelIndex &left, const QModelIndex &right) const final;
+
+    bool sortETA(
+            const QModelIndex &left, const QModelIndex &right,
+            const QVariant &leftRawValue, const QVariant &rightRawValue) const;
+    bool sortSeedsAndLeechers(
+            const QModelIndex &left, const QModelIndex &right,
+            const QVariant &leftRawValue, const QVariant &rightRawValue) const;
+
+    /*! Invoke lessThan() for a passed column, used in the second sort (recursion). */
+    inline bool
+    invokeLessThanForColumn(
+            TorrentSqlTableModel::Column column,
+            const QModelIndex &left, const QModelIndex &right) const;
 };
+
+bool TorrentTableSortModel::invokeLessThanForColumn( // NOLINT(misc-no-recursion)
+        const TorrentSqlTableModel::Column column,
+        const QModelIndex &left, const QModelIndex &right) const
+{
+    return lessThan(left.sibling(left.row(), column),
+                    right.sibling(right.row(), column));
+}
 
 #endif // TORRENTTABLESORTMODEL_H
